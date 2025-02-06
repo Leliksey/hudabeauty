85de342ce11235d844cd5dabd554d4d3621d0122
Leliksey <captainfroody@gmail.com>
2025-02-06 04:59:04 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,166 @@
+$(document).ready(function() {
+    const slider = $(".slider-item");
+
+    function onSliderAfterChange(event, slick, currentSlide) {
+        $(event.target).data('current-slide', currentSlide);
+    }
+    
+    function onSliderWheel(e) {
+        var deltaY = e.originalEvent.deltaY,
+        $currentSlider = $(e.currentTarget),
+        currentSlickIndex = $currentSlider.data('current-slide') || 0;
+        
+        if (
+        // check when you scroll up
+        (deltaY < 0 && currentSlickIndex == 0) ||
+        // check when you scroll down
+        (deltaY > 0 && currentSlickIndex == $currentSlider.data('slider-length') - 1)
+        ) {
+        return;
+        }
+
+        e.preventDefault();
+
+        if (e.originalEvent.deltaY < 0) {
+        $currentSlider.slick('slickPrev');
+        } else {
+        $currentSlider.slick('slickNext');
+        }
+    }
+    
+    slider.each(function(index, element) {
+        var $element = $(element);
+        // set the length of children in each loop
+        // but the better way for performance is to set this data attribute on the div.slider in the markup
+        $element.data('slider-length', $element.children().length);
+    })
+    .slick({
+        infinite: true,
+        slidesToShow: 9,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        vertical: true,
+        centerMode: true,
+        centerPadding: 0,
+        responsive: [
+            {
+                breakpoint: 1024, // При ширине экрана <= 1024px
+                settings: {
+                    vertical: false,
+                }
+            },
+        ]
+    })
+    .on('afterChange', onSliderAfterChange)
+    .on('wheel', onSliderWheel);
+
+    function killSlider() {
+        if($(window).width() < 1025 ) {
+            $(slider).slick('unslick');
+            $(".slider-item-img").removeClass("active")
+        }
+    }
+    killSlider()
+
+
+
+    $(".slider-item-img").on("click", function() {
+        let src = $(this).find("img").attr("src");
+        $(".main__img img").attr("src", src);
+        $(".slider-item-img").removeClass("active");
+        $(this).addClass("active");
+    })
+    $(".productInfo__heading svg").on('click', function() {
+        $(this).toggleClass("active")
+    });
+
+    function formatDate(date) {
+        let day = date.getDate().toString().padStart(2, '0');
+        let month = (date.getMonth() + 1).toString().padStart(2, '0'); // Январь — 0, поэтому +1
+        return `${day}.${month}`;
+    }
+
+    let today = new Date();
+    let fromDate = new Date();
+    fromDate.setDate(today.getDate() - 3); // Отнимаем 3 дня
+
+    $("#from").text(formatDate(fromDate));
+    $("#to").text(formatDate(today));
+
+
+
+    $(".product__tabs-item").click(function() {
+        // $(this).parent().find(".product__tabs-item").removeClass("active"); 
+        // $(this).addClass("active");
+        $(this).next().slideToggle();
+    });
+    $(document).on("click", ".product__tabs-item", function(e) {
+        e.preventDefault();
+        $(this).toggleClass("open");
+      });
+
+
+
+      let hasScrolled = false;
+
+        $(window).on("scroll", function () {
+            if ($(this).scrollTop() > 150 && !hasScrolled) {
+                hasScrolled = true;
+                onScrollDown(); // Функция при начале скроллинга вниз
+            } else if ($(this).scrollTop() === 0 && hasScrolled) {
+                hasScrolled = false;
+                onScrollTop(); // Функция при возврате наверх
+            }
+        });
+
+        function onScrollDown() {
+            $(".header").addClass("fix")
+        };
+
+        function onScrollTop() {
+            $(".header").removeClass("fix")
+        };
+
+
+
+
+        $(".overlay, .popup__close").on("click", function() {
+            $(".overlay").fadeOut(300);
+            $(".popup").removeClass("open");
+            $("body").removeClass("ovh");
+        });
+        $(".productInfo__delivery").on("click", function() {
+            $(".overlay").fadeIn(300);
+            $("body").addClass("ovh");
+            $(".popup__delivery").addClass("open");
+        });
+
+
+        function setSliderHeight() {
+            let mainImgHeight = $(".main__img img").height();
+            if (mainImgHeight && $(window).width() > 1024 ) {
+                $(".slider-item").height(mainImgHeight);
+            }
+        }
+
+
+        function initOwl() {
+            if($(window).width() < 1025 ) {
+                $(".slider-item").owlCarousel({
+                    loop: true,
+                    nav: false,
+                    dots: true,
+                    items: 1
+                });
+            }
+        }
+        initOwl()
+        setSliderHeight();
+        $(window).on("resize", function() {
+            setSliderHeight();
+            killSlider();
+            $(".slider-item").trigger('destroy.owl.carousel').removeClass('owl-carousel owl-loaded');;
+            initOwl();
+        });
+});
---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/font/Futura/stylesheet.css">
+    <link rel="stylesheet" href="assets/font/Helvetica/stylesheet.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <title>Huda Beauty</title>
+</head>
+<body>
+    <header class="header">
+        <div class="header__top">
+            Free Sample On Every Order
+        </div>
+        <div class="header__middle">
+            <div class="container_1280">
+                <div class="header__middle-content">
+                    <div class="mobile">
+                        <div class="header__middle-content_mobile">
+                            <svg class="burger" width="25px" height="25px" viewBox="0 0 12 12" enable-background="new 0 0 12 12" id="Слой_1" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g>
+                                <rect fill="#fff" height="1" width="11" x="0.5" y="5.5"/>
+                                <rect fill="#fff" height="1" width="11" x="0.5" y="2.5"/>
+                                <rect fill="#fff" height="1" width="11" x="0.5" y="8.5"/>
+                                </g>
+                            </svg>
+                            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="header__middle-lang">
+                        Poland | EN 
+                    </div>
+                    <div class="header__middle-logo">
+                        <img src="assets/img/logo.svg" alt="">
+                    </div>
+                    <ul class="menu">
+                        <li class="menu__item">
+                            <a href="#" class="menu__link">New</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="#" class="menu__link">Best Sellers</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="#" class="menu__link">Huda Beauty</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="#" class="menu__link">Kayali</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="#" class="menu__link">wishful</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="#" class="menu__link">Gifts & Sets</a>
+                        </li>
+                    </ul>
+                    <div class="header__middle-icons">
+                        <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_15_152)">
+                            <rect width="24" height="24" fill="#eb3986"/>
+                            <circle cx="10.5" cy="10.5" r="6.5" stroke="#fff" stroke-linejoin="round"/>
+                            <path d="M19.6464 20.3536C19.8417 20.5488 20.1583 20.5488 20.3536 20.3536C20.5488 20.1583 20.5488 19.8417 20.3536 19.6464L19.6464 20.3536ZM20.3536 19.6464L15.3536 14.6464L14.6464 15.3536L19.6464 20.3536L20.3536 19.6464Z" fill="#fff"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_15_152">
+                            <rect width="24" height="24" fill="#eb3986"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+                        <svg class="desktop" width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <svg width="25px" height="25px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" stroke-width="3" stroke="#fff" fill="none"><path d="M9.06,25C7.68,17.3,12.78,10.63,20.73,10c7-.55,10.47,7.93,11.17,9.55a.13.13,0,0,0,.25,0c3.25-8.91,9.17-9.29,11.25-9.5C49,9.45,56.51,13.78,55,23.87c-2.16,14-23.12,29.81-23.12,29.81S11.79,40.05,9.06,25Z"/>
+                        </svg>
+                        <img src="assets/img/cart.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header__bottom">
+            <ul class="menu">
+                <li class="menu__item">
+                    <a href="#" class="menu__link">New</a>
+                </li>
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Best Sellers</a>
+                </li>
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Huda Beauty</a>
+                </li>
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Kayali</a>
+                </li>
+                <li class="menu__item">
+                    <a href="#" class="menu__link">wishful</a>
+                </li>
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Gifts & Sets</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <main class="main">
+        <div class="container_1540">
+            <ul class="breadcrumbs">
+                <li class="breadcrumbs__item"><a href="#">Shop</a></li>
+                <li class="breadcrumbs__item"><a href="#">New</a></li>
+            </ul>
+            <section class="sectionProduct">
+                <div class="sectionProduct__sliders">
+                    <div class="slider-item">
+                        <div class="slider-item-img active"><img src="assets/img/girl_1.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/girl_2.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/girl_3.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/granites.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/flowers.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/klubnikes.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/mylos.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/parfume.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/parfumes_klubnikes.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/parfumes.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/people.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/rafaello.jpg" alt=""></div>
+                        <div class="slider-item-img"><img src="assets/img/zefires.jpg" alt=""></div>
+                    </div>
+                    <div class="main__img">
+                        <img src="assets/img/girl_1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="productInfo">
+                    <div class="productInfo__heading">
+                        <div class="productInfo__title">Yum Boujee Marshmallow | 81 Eau de Parfum Intense</div>
+                        <svg width="40px" height="40px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" stroke-width="3" stroke="#000" fill="none"><path d="M9.06,25C7.68,17.3,12.78,10.63,20.73,10c7-.55,10.47,7.93,11.17,9.55a.13.13,0,0,0,.25,0c3.25-8.91,9.17-9.29,11.25-9.5C49,9.45,56.51,13.78,55,23.87c-2.16,14-23.12,29.81-23.12,29.81S11.79,40.05,9.06,25Z"/>
+                        </svg>
+                    </div>
+                    <img class="trustpilot" src="assets/img/trustpilot.svg" alt="">
+                    <div class="productInfo__rating">
+                        <div class="stars">
+                            <div class="star"></div>
+                            <div class="star"></div>
+                            <div class="star"></div>
+                            <div class="star"></div>
+                            <div class="star"></div>
+                        </div>
+                        <a href="#reviews" class="productInfo__rating-result">4.9(814 Reviews)</a>
+                    </div>
+                    <div class="productInfo__price">
+                        <div class="price__old">€130.00</div>
+                        <div class="price__new">€100.00
+                            <span class="vat">Including VAT</span></div>
+                    </div>
+                    <div class="productInfo__action">
+                        <button class="order">buy now</button>
+                        <div class="productInfo__count">
+
+                        </div>
+                    </div>
+                    <div class="productInfo__dates">
+                        🎉 Special Deal: Valid from <span id="from"></span> - <span id="to"></span>
+                    </div>
+                    <div class="productInfo__gift">
+                        <img src="assets/img/gift.png" alt="">
+                        Buy now to earn 130 points
+                    </div>
+                    <div class="productInfo__tabs">
+                    <?php
+                        $macrosFile = 'macros_description.txt';
+
+                        // Считывание макросов из файла и формирование массива данных для вкладок и таблиц
+                        $tabs = [];
+                        $contents = [];
+                        if (file_exists($macrosFile)) {
+                            $lines = file($macrosFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+                            foreach ($lines as $line) {
+                                list($key, $title, $description) = explode(';', $line, 3);
+                                $tab_number = preg_replace('/description_([0-9]+)_sheets$/', '$1', $key);
+                                $tab_number = str_replace('description_', '', $tab_number);  // Номер вкладки без слова "description"
+                                $tabs[$tab_number] = trim($title);
+
+                                // Проверяем, содержит ли ключ слово "sheets"
+                                if (strpos($key, '_sheets') !== false) {
+                                    // Генерация таблицы
+                                    $rows = explode('|', trim($description));
+                                    $table_html = '<div class="product__tabs-info' . ($tab_number == 1 ? '' : ' ') . '"><table>';
+                                    foreach ($rows as $row) {
+                                        $cells = explode(';;', $row);
+                                        $table_html .= '<tr><td>' . htmlspecialchars(trim($cells[0])) . '</td><td>' . htmlspecialchars(trim($cells[1])) . '</td></tr>';
+                                    }
+                                    $table_html .= '</table></div>';
+                                    $contents[$tab_number] = $table_html;
+                                } else {
+                                    // Обычное описание
+                                    $contents[$tab_number] = '<div class="product__tabs-info' . ($tab_number == 1 ? '' : ' ') . '"><p class="text_descr">' . htmlspecialchars_decode(trim($description)) . '</p></div>';
+                                }
+                            }
+                        }
+
+                        // Вывод вкладок и их содержимого
+                        echo '<div class="wrap_product_additionals"><ul class="product__tabs">';
+                        foreach ($tabs as $number => $title) {
+                            $active_class = $number == 1 ? ' open active' : ''; // Активный класс только для первой вкладки
+                            echo "<li class='product__tabs-item$active_class'>$title</li>";
+                            echo $contents[$number]; // Выводим соответствующее содержимое
+                        }
+                        echo "</ul>";
+                        echo '</div>';
+                        echo '</ul>';
+                        ?>
+                    </div>
+                    <div class="productInfo__delivery">View shipping and returns options</div>
+                </div>
+            </section>
+        </div>
+    </main>
+    <footer>
+        <div class="popup popup__delivery">
+            <div class="popup__heading">
+                <div class="popup__title">Shipping Information</div>
+                <div class="popup__close">
+                    <img src="assets/img/close.svg" alt="">
+                </div>
+            </div>
+            <div class="popup__content">
+                <div class="popup__subtitle">Shipping Info:</div>
+                <div class="popup__desc">When we receive your order, it will be processed in our France warehouse within 1-2 business days (weekends and holidays are not included). Once completed, delivery timeframe is 3-5 business days.</div>
+                <div class="popup__desc">Please consider the above as guidelines, these may be subject to change. Once an order is completed, you will receive your tracking details via email. Please allow 1 business day for your tracking number to activate.</div>
+                <div class="popup__subtitle">Returns:</div>
+                <div class="popup__desc">If you are not satisfied with your purchase, you may return your item for free within 60 days, with the exception of fragrance and other items listed on our returns FAQ (<a href="#">click HERE</a>). To arrange returns, please contact us at <a href="mailto:shop@hudabeauty.com">shop@hudabeauty.com</a>. You will be refunded for the amount paid for the product, excluding any shipping costs.</div>
+            </div>
+        </div>
+        <div class="overlay"></div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
